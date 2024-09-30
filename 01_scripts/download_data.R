7cb39ec0f908e4549be85002ff17863fac4b0213
Andrew <your.email@example.com>
2024-09-30 15:02:45 -0400

    Maked changes to OSF

---
+++ 01_scripts/download_data.R
@@ -0,0 +1,12 @@
+#Andrew Vermey
+#This is for raw data for my Productivity course
+
+
+#Downloading dataset and saving varible
+dataset <- read.csv("https://raw.github.com/vincentarelbundock/Rdatasets/master/csv/datasets/AirPassengers.csv") 
+
+#Saving the data to a rawdata folder
+write.csv(dataset, "./00_rawdata/data.csv") 
+
+#later in RMarkdown load this data and put as a plot and table and add some comments
+#later make a data dictionary in the raw_data folder explaining my data

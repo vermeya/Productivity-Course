ab8eeef2159a8bb84f5ef9d42b1b67fd2e94442f
Andrew <your.email@example.com>
2024-10-01 00:03:03 -0400

    This is a test for git commit

--- 01_scripts/download_data.R
+++ 01_scripts/download_data.R
@@ -10,3 +10,7 @@ write.csv(dataset, "./00_rawdata/data.csv")
 
 #later in RMarkdown load this data and put as a plot and table and add some comments
 #later make a data dictionary in the raw_data folder explaining my data
+
+#add to git (git add .)
+#commit (git commit -m "This is a test for git commit")
+#push (git push)
